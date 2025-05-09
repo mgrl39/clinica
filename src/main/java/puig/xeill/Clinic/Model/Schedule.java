f237222010eb40de5c2688ccb5a47b8e253dfab7
Rubén <rjulian@elpuig.xeill.net>
2025-05-09 17:23:47 +0200

    putos enums de lso huevos

--- src/main/java/puig/xeill/Clinic/Model/Schedule.java
+++ src/main/java/puig/xeill/Clinic/Model/Schedule.java
@@ -1,9 +1,6 @@
 package puig.xeill.Clinic.Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,11 +19,14 @@ public class Schedule {
     @Id
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "firstDay")
     private WeekDay firstDay;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "lastDay")
     private WeekDay lastDay;
+
 
     @Column(name = "firstHour")
     private Date firstHour;
