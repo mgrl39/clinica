46b3d3edc636f07bfca446ace3897eb73643f9de
ruben <rubenjulian2003@gmail.com>
2025-05-26 05:00:09 +0200

    la politziaaaaaaaaaaa

--- src/main/java/puig/xeill/Clinic/Model/Enums/WeekDay.java
+++ src/main/java/puig/xeill/Clinic/Model/Enums/WeekDay.java
@@ -1,9 +1,31 @@
 package puig.xeill.Clinic.Model.Enums;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public enum WeekDay {
     MONDAY,
     TUESDAY,
     WEDNESDAY,
     THURSDAY,
-    FRIDAY
+    FRIDAY;
+
+
+    public static WeekDay fromDate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        // Mapear Calendar.DAY_OF_WEEK a WeekDay, asumiendo que WeekDay empieza en MONDAY=0
+        switch (dayOfWeek) {
+            case Calendar.MONDAY: return WeekDay.MONDAY;
+            case Calendar.TUESDAY: return WeekDay.TUESDAY;
+            case Calendar.WEDNESDAY: return WeekDay.WEDNESDAY;
+            case Calendar.THURSDAY: return WeekDay.THURSDAY;
+            case Calendar.FRIDAY: return WeekDay.FRIDAY;
+            default: throw new IllegalArgumentException("Invalid day of week");
+        }
+    }
 }
+
+
+
